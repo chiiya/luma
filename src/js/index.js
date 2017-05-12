22de0609180f0c4f2c7150c4146bf5a4784abb00
Elisha Witte <elishawitte@gmail.com>
2017-05-12 13:08:02 +0200

    Added modals.

--- src/js/index.js
+++ src/js/index.js
@@ -9,6 +9,10 @@ function displayFileName(el) {
   el.nextElementSibling.innerHTML = `File: ${el.value.replace(/^.*?([^\\/]*)$/, '$1')}`;
 }
 
+function hasClass(target, className) {
+  return new RegExp('(\\s|^)' + className + '(\\s|$)').test(target.className);
+}
+
 // Resize textareas on input
 if (document.getElementsByTagName('textarea').length > 0) {
   document.body.addEventListener('input', (event) => {
@@ -31,3 +35,41 @@ if (document.getElementsByClassName('file').length > 0) {
 if (document.getElementsByClassName('select').length > 0) {
   styleSelect('.select');
 }
+
+if (document.getElementsByClassName('lm-modal').length > 0) {
+
+  // Open Modal
+  [].forEach.call(document.querySelectorAll('.lm-modal-trigger'), (modal) => {
+    modal.addEventListener('click', (e) => {
+      e.preventDefault();
+      document.querySelector('.lm-modal').classList.add('is-visible');
+    }, false);
+  });
+
+  // Close Modal
+  [].forEach.call(document.querySelectorAll('.lm-modal'), (modal) => {
+    modal.addEventListener('click', (e) => {
+      if (hasClass(e.target, 'lm-modal-close') || hasClass(e.target, 'lm-modal')
+        || hasClass(e.target, 'lm-close')) {
+        e.preventDefault();
+        modal.classList.remove('is-visible');
+      }
+    }, false);
+  });
+
+  // Close Modal by ESC with browser fallback
+  document.onkeydown = (event) => {
+    const e = event || window.event;
+    let isEscape = false;
+    if ('key' in e) {
+      isEscape = (e.key === 'Escape' || e.key === 'Esc');
+    } else {
+      isEscape = (e.keyCode === 27);
+    }
+    if (isEscape) {
+      document.querySelector('.lm-modal').classList.remove('is-visible');
+    }
+  };
+}
+
+
